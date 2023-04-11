162c4aa4951deecdbadd3d197b37e8c09cc55cb0
DuatTran268 <duatsen36@gmail.com>
2023-04-11 23:20:45 +0700

    merge from Hung

--- src/back-end/ManageProject.Data/Contexts/ManageDdContext.cs
+++ src/back-end/ManageProject.Data/Contexts/ManageDdContext.cs
@@ -1,6 +1,7 @@
 ﻿using ManageProject.Core.Entities;
 using ManageProject.Data.Mappings;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,41 +10,51 @@ using System.Threading.Tasks;
 
 namespace ManageProject.Data.Contexts
 {
-    public class ManageDbContext : DbContext
-    {
-        public DbSet<CheckProcess> CheckProcesses { get; set; }
+	public class ManageDbContext : DbContext
+	{
+		public DbSet<CheckProcess> CheckProcesses { get; set; }
 
-        public DbSet<Department> Departments { get; set; }
+		public DbSet<Department> Departments { get; set; }
 
-        public DbSet<Post> Posts { get; set; }
+		public DbSet<Post> Posts { get; set; }
 
-        public DbSet<Process> Processs { get; set; }
+		public DbSet<Process> Processs { get; set; }
 
-        public DbSet<Project> Projects { get; set; }
+		public DbSet<Project> Projects { get; set; }
 
-        public DbSet<Role> Roles { get; set; }
+		public DbSet<Role> Roles { get; set; }
 
-        public DbSet<User> Users { get; set; }
+		public DbSet<User> Users { get; set; }
 
-        public ManageDbContext(DbContextOptions<ManageDbContext> options):base(options) {
-        
-        }
-        public ManageDbContext()
-        {
+		public ManageDbContext(DbContextOptions<ManageDbContext> options) : base(options)
+		{
 
-      
-            optionsBuilder.UseSqlServer(@"Server=XUANHUNG;Database=NCKHGV;Trusted_Connection=True;MultipleActiveResultSets=True;TrustServerCertificate=True;");
+		}
+		public ManageDbContext()
+		{
 
 
-        }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.ApplyConfigurationsFromAssembly(typeof(DepartmentMap).Assembly);
-        }
+			//optionsBuilder.UseSqlServer(@"Server=DESKTOP-JS68JVN\SQLEXPRESS;Database=NCKHGV;Trusted_Connection=True;MultipleActiveResultSets=True;TrustServerCertificate=True;");
 
 
-        // test
+		}
 
-    }
+		protected override void OnConfiguring(
+			DbContextOptionsBuilder optionsBuilder)
+		{
+
+			optionsBuilder.UseSqlServer(@"Server=DESKTOP-JS68JVN\SQLEXPRESS;Database=NCKHGiangVien;Trusted_Connection=True;MultipleActiveResultSets=True;TrustServerCertificate=True;");
+			// server name : DESKTOP-JS68JVN\SQLEXPRESS
+
+		}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.ApplyConfigurationsFromAssembly(typeof(DepartmentMap).Assembly);
+		}
+
+
+		// test
+
+	}
 }
