9cdc49448a008ff55c0b599f88a9fbd883f47127
DuatTran268 <duatsen36@gmail.com>
2023-05-08 18:55:04 +0700

    get api display post

--- src/front-end/src/components/user/manage/departments/Department.jsx
+++ src/front-end/src/components/user/manage/departments/Department.jsx
@@ -1,29 +1,51 @@
 import { faBank } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import "../Manage.scss"
+import "../Manage.scss";
+import { getAllPost } from "../../../../api/PostApi";
 
 const Departments = () => {
+  const [getPost, setGetPost] = useState([]);
+
+  useEffect(() => {
+    getAllPost().then((data) => {
+      console.log("data check: ", data);
+      if (data) {
+        setGetPost(data);
+      } else {
+        setGetPost([]);
+      }
+    });
+  }, []);
+
   return (
     <>
       <div className="card">
-        <FontAwesomeIcon icon={faBank} fontSize={80} className="text-danger py-3"/>
+        <FontAwesomeIcon
+          icon={faBank}
+          fontSize={80}
+          className="text-danger py-3"
+        />
         <h5 className="text-success text-center">Phòng Khoa</h5>
-        
+
         <div className="card-body">
-          <div className="card-content">
-            <Link className="text-decoration-none">
-              <h6>Quá trình chuyển biến kinh tế xã hội của thành phố Đà Lạt</h6>
-            </Link>
-            <div className="card-shortdesc">Mô tả ngắn về bài báo nghiên cứu khoa học</div>
-            <div className="card-author">
-              <span className="card-author-title">Tác giả:</span>
+          {getPost.map((item, index) => (
+            <div className="card-content mt-1" key={index}>
               <Link className="text-decoration-none">
-                <span className="card-author-name px-3">Duật Trần</span>
+                <h6>{item.title}</h6>
               </Link>
+              <div className="card-shortdesc">{item.shortDescription}</div>
+              <div className="card-author">
+                <span className="card-author-title">Tác giả:</span>
+                <Link className="text-decoration-none">
+                  <span className="card-author-name px-3">
+                    {item.user.name}
+                  </span>
+                </Link>
+              </div>
             </div>
-          </div>
+          ))}
         </div>
       </div>
     </>
