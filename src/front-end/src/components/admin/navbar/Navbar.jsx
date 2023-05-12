2d503b397b1b6dd408a46c0f4aa0ff6b03249d3b
DuatTran268 <duatsen36@gmail.com>
2023-05-12 11:34:32 +0700

    ui admin

--- src/front-end/src/components/admin/navbar/Navbar.jsx
+++ src/front-end/src/components/admin/navbar/Navbar.jsx
@@ -1,15 +1,21 @@
 import React from "react";
-
-
-
+import { Link } from "react-router-dom";
+import "../stylead.scss";
 
 const Navbar = () => {
   return (
     <>
-      <div className="bg-success text-white">
-        <h1>DDay la admin</h1>
+      <div className="item-admin text-end px-5 py-3">
+        <Link className="text-decoration-none">
+          <img
+            src="https://tse2.mm.bing.net/th?id=OIP.Lfc-ED0sVK9GOkjPJjB2cwAAAA&pid=Api&P=0"
+            alt=""
+            className="avatar"
+          />
+          <span className="px-2">Admin</span>
+        </Link>
       </div>
     </>
-  )
-}
+  );
+};
 export default Navbar;
--- src/front-end/src/components/admin/sidebar/Sidebar.jsx
+++ src/front-end/src/components/admin/sidebar/Sidebar.jsx
@@ -1,84 +1,107 @@
 import {
   faArrowCircleUp,
-  faBackwardFast,
   faBook,
-  faBookOpen,
   faCircleCheck,
   faDashboard,
   faHomeAlt,
   faPen,
   faPhone,
-  faPodcast,
   faSignOut,
   faUser,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import { Link } from "react-router-dom";
+import "../stylead.scss";
 
 const Sidebar = () => {
   return (
     <>
-      <div className="sidebar bg-danger text-white h-100">
+      <div className="sidebar">
         <ul>
-          <p className="sidebar-tile">Main</p>
-          <Link className="text-decoration-none" to={`/admin`}>
-            <li>
-              <FontAwesomeIcon icon={faDashboard} />
-              <span className="px-3">Dashboard</span>
-            </li>
-          </Link>
-          <p className="sidebar-tile">List</p>
-          <Link className="text-decoration-none" to={`/admin/researcher`}>
-            <li>
-              <FontAwesomeIcon icon={faUser} />
-              <span className="px-3">Nhà khoa học</span>
-            </li>
-          </Link>
-          <Link className="text-decoration-none" to={`/admin/project`}>
-            <li>
-              <FontAwesomeIcon icon={faBook} />
-              <span className="px-3">Dự án</span>
-            </li>
-          </Link>
-          <Link className="text-decoration-none">
-            <li>
-              <FontAwesomeIcon icon={faPen} />
-              <span className="px-3">Bài đăng</span>
-            </li>
-          </Link>
-          <Link className="text-decoration-none">
-            <li>
-              <FontAwesomeIcon icon={faHomeAlt} />
-              <span className="px-3">Phòng khoa</span>
-            </li>
-          </Link>
-          <Link className="text-decoration-none">
-            <li>
-              <FontAwesomeIcon icon={faArrowCircleUp} />
-              <span className="px-3">Tiến trình</span>
-            </li>
-          </Link>
-          <Link className="text-decoration-none">
-            <li>
-              <FontAwesomeIcon icon={faPhone} />
-              <span className="px-3">Liên hệ</span>
-            </li>
-          </Link>
-          <p className="sidebar-tile">Service</p>
-          <Link className="text-decoration-none">
-            <li>
-              <FontAwesomeIcon icon={faCircleCheck} />
-              <span className="px-3">Cài đặt</span>
-            </li>
-          </Link>
-          <Link className="text-decoration-none" to={`/`}>
-            <li>
-              <FontAwesomeIcon icon={faSignOut} />
-              <span className="px-3">Đăng xuất</span>
-            </li>
-          </Link>
-          
+          <div className="sidebar-main">
+            <span className="sidebar-tile">Main</span>
+            <Link className="sidebar-link" to={`/admin`}>
+              <li>
+                <FontAwesomeIcon icon={faDashboard} />
+                <span className="px-3">Dashboard</span>
+              </li>
+            </Link>
+          </div>
+
+          <div className="sidebar-main">
+            <span className="sidebar-tile">List</span>
+            <div className="sidebar-wrapper">
+              <Link className="sidebar-link" to={`/admin/researcher`}>
+                <li>
+                  <FontAwesomeIcon icon={faUser} />
+                  <span className="px-3">Nhà khoa học</span>
+                </li>
+              </Link>
+            </div>
+            <div className="sidebar-wrapper">
+              <Link className="sidebar-link" to={`/admin/project`}>
+                <li>
+                  <FontAwesomeIcon icon={faBook} />
+                  <span className="px-3">Dự án</span>
+                </li>
+              </Link>
+            </div>
+
+            <div className="sidebar-wrapper">
+              <Link className="sidebar-link">
+                <li>
+                  <FontAwesomeIcon icon={faPen} />
+                  <span className="px-3">Bài đăng</span>
+                </li>
+              </Link>
+            </div>
+
+            <div className="sidebar-wrapper">
+              <Link className="sidebar-link">
+                <li>
+                  <FontAwesomeIcon icon={faHomeAlt} />
+                  <span className="px-3">Phòng khoa</span>
+                </li>
+              </Link>
+            </div>
+            <div className="sidebar-wrapper">
+              <Link className="sidebar-link">
+                <li>
+                  <FontAwesomeIcon icon={faArrowCircleUp} />
+                  <span className="px-3">Tiến trình</span>
+                </li>
+              </Link>
+            </div>
+            <div className="sidebar-wrapper">
+              <Link className="sidebar-link">
+                <li>
+                  <FontAwesomeIcon icon={faPhone} />
+                  <span className="px-3">Liên hệ</span>
+                </li>
+              </Link>
+            </div>
+          </div>
+
+          <div className="sidebar-main">
+            <span className="sidebar-tile">Service</span>
+            <div className="sidebar-wrapper">
+              <Link className="sidebar-link">
+                <li>
+                  <FontAwesomeIcon icon={faCircleCheck} />
+                  <span className="px-3">Cài đặt</span>
+                </li>
+              </Link>
+            </div>
+            <div className="sidebar-wrapper">
+              <Link className="sidebar-link" to={`/`}>
+                <li>
+                  <FontAwesomeIcon icon={faSignOut} />
+                  <span className="px-3">Đăng xuất</span>
+                </li>
+              </Link>
+            </div>
+          </div>
         </ul>
       </div>
     </>
