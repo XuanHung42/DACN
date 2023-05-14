ee31d0e4cfba8be950ed201fc3e94aabd6ec3ef5
Xuan Hung <79842226+XuanHung42@users.noreply.github.com>
2023-05-14 07:56:53 +0700

    Fix add project

--- src/front-end/src/components/user/home/Department.jsx
+++ src/front-end/src/components/user/home/Department.jsx
@@ -27,17 +27,21 @@ const Departments = () => {
           fontSize={80}
           className="text-danger py-3"
         />
-        <h5 className="text-success text-center">Phòng Khoa</h5>
-
+        <h5 className="text-success text-center">Phòng Khoa</h5>A
         <div className="card-body">
           {getPost.map((item, index) => (
             <div className="card-content mt-1" key={index}>
-<Link className="text-decoration-none" to={`/${item.urlSlug}`}>                <h5>{item.title}</h5>
+              <Link className="text-decoration-none" to={`/${item.urlSlug}`}>
+                {" "}
+                <h5>{item.title}</h5>
               </Link>
               <div className="card-shortdesc">{item.shortDescription}</div>
               <div className="card-author">
                 <span className="card-author-title">Đăng bởi: </span>
-                <Link className="text-decoration-none px-2" to={`/home/${item.user.urlSlug}`}>
+                <Link
+                  className="text-decoration-none px-2"
+                  to={`/home/${item.user.urlSlug}`}
+                >
                   {item.user.name}
                 </Link>
               </div>
