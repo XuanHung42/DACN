5b8d9e78c4ab2b22846455a3221cd9c243f29e87
DuatTran268 <duatsen36@gmail.com>
2023-05-15 22:16:55 +0700

    commit code

--- src/front-end/src/components/admin/dashboard/Dashboard.jsx
+++ src/front-end/src/components/admin/dashboard/Dashboard.jsx
@@ -1,3 +1,6 @@
+import { faFaceGrinHearts, faUser } from "@fortawesome/free-regular-svg-icons";
+import { faList, faPhone, faTruck } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 
 
@@ -5,7 +8,43 @@ import React from "react";
 const DashboardAdmin = () => {
   return (
     <>
-      <h1> Đây là khu vực quản lý admin</h1>
+      <div className="d-flex mt-5">
+        <div className="card-body">
+          <div className="text-success">
+            <h3>Số user</h3>
+            <FontAwesomeIcon icon={faUser} fontSize={30} />
+            <span className="text-danger px-5 display-6">8</span>
+          </div>
+        </div>
+        <div className="card-body">
+          <div className="text-success">
+            <h3>Loại dịch vụ</h3>
+            <FontAwesomeIcon icon={faList} fontSize={30} />
+            <span className="text-danger px-5 display-6">8</span>
+          </div>
+        </div>
+        <div className="card-body">
+          <div className="text-success">
+            <h3>Dịch vụ</h3>
+            <FontAwesomeIcon icon={faFaceGrinHearts} fontSize={30} />
+            <span className="text-danger px-5 display-6">8</span>
+          </div>
+        </div>
+        <div className="card-body">
+          <div className="text-success">
+            <h3>Hỗ trợ</h3>
+            <FontAwesomeIcon icon={faPhone} fontSize={30} />
+            <span className="text-danger px-5 display-6">6</span>
+          </div>
+        </div>
+        <div className="card-body">
+          <div className="text-success">
+            <h3>Đặt hàng</h3>
+            <FontAwesomeIcon icon={faTruck} fontSize={30} />
+            <span className="text-danger px-5 display-6">2</span>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
